8c0f21809e5a32bf62ed71c553b85084a480460a
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-09 20:46:04 +0200

    Add Native Query to Jpa Repo

--- src/main/java/com/api/BooksApiWithMySql/repository/BooksJpaRepository.java
+++ src/main/java/com/api/BooksApiWithMySql/repository/BooksJpaRepository.java
@@ -2,6 +2,7 @@ package com.api.BooksApiWithMySql.repository;
 
 import com.api.BooksApiWithMySql.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,6 @@ import java.util.List;
 public interface BooksJpaRepository extends JpaRepository<Book, Long> {
     List<Book> findByTitleContainingOrAuthorContaining(String titleKeyword, String authorKeyword);
 
+    @Query(nativeQuery = true, value = "Select * from books where quantity = '0'")
+    List<Book> findOutOfStockBooks();
 }
