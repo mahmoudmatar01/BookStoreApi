af909c351d73e9e561cc8f96cf006ed5b15b8d69
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-12 18:30:03 +0200

    Refactor code of `BooksJpaRepository`

--- src/main/java/com/api/BooksApiWithMySql/repository/BooksJpaRepository.java
+++ src/main/java/com/api/BooksApiWithMySql/repository/BooksJpaRepository.java
@@ -2,28 +2,17 @@ package com.api.BooksApiWithMySql.repository;
 
 import com.api.BooksApiWithMySql.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BooksJpaRepository extends JpaRepository<Book, Long> {
-    List<Book> findByTitleContainingOrAuthorContaining(String titleKeyword, String authorKeyword);
-
-//    @Query(nativeQuery = true, value = "Select * from books where quantity = '0'")
-//    List<Book> findOutOfStockBooks();
-
-    @Query("SELECT t FROM Book t WHERE t.price BETWEEN :start AND :end order by t.price DESC ")
-    List<Book> findBooksBetweenPrice(@Param("start") double start, @Param("end") double end);
-
-    @Query("select t from Book t where t.author like %:author_name% ORDER BY t.price desc ")
-    List<Book> findBookByAuthor(@Param("author_name") String author);
+    List<Book> findBooksByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(String title, String author);
 
     List<Book> findBooksByPriceBetweenOrderByPriceDesc(double price, double price2);
 
-    List<Book> findBookByTitleLikeOrderByPriceDesc(String title);
+    Book findBookByTitleLike(String title);
 
     List<Book> findBookByAuthorLikeOrderByPriceDesc(String author);
 }
