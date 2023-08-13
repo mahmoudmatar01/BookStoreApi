da58ec534815409dfe4e042a62424bca66a0679d
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-13 12:13:01 +0200

    Add custom exception handler for validation request body.

--- src/main/java/com/api/BooksApiWithMySql/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/api/BooksApiWithMySql/exceptions/ControllerExceptionHandler.java
@@ -3,11 +3,19 @@ package com.api.BooksApiWithMySql.exceptions;
 import com.api.BooksApiWithMySql.factories.FailureFactory;
 import com.api.BooksApiWithMySql.responses.FailureResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 @ResponseBody
@@ -21,11 +29,24 @@ public class ControllerExceptionHandler {
     }
 
 
-
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public FailureResponse globalException(Exception ex ,WebRequest request){
+    public FailureResponse globalException(Exception ex, WebRequest request) {
         FailureFactory factory = new FailureFactory(ex.getMessage(), 500);
         return (FailureResponse) factory.createResponse();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> notValidRequestBody(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request) {
+        List<String> errors = new ArrayList<>();
+
+        ex.getAllErrors().forEach(err -> errors.add(err.getDefaultMessage()));
+
+        Map<String, List<String>> result = new HashMap<>();
+        result.put("errors", errors);
+
+        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
     }
 }
