27853300a72ff7b4e895d6a03bb68292fb128eeb
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-15 13:37:01 +0200

    Add `JwtService` for JWT Utils.

---
+++ src/main/java/com/api/BooksApiWithMySql/config/JwtService.java
@@ -0,0 +1,75 @@
+package com.api.BooksApiWithMySql.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "7ab42c0c898fa0c4612166e5f829e935e2ee3084b329bd15579e525844cc3593";
+
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigninKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Key getSigninKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
+    }
+
+    public String generateToken(
+            Map<String, Object> claims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 24 * 60))
+                .signWith(getSigninKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public boolean isValidToken(String token, UserDetails userDetails) {
+        String userName = extractUserName(token);
+        return userName.equals(userDetails.getUsername()) && !isExpiredToken(token);
+    }
+
+    private boolean isExpiredToken(String token) {
+        return Objects.requireNonNull(extractExpirationDate(token)).before(new Date());
+    }
+
+    private Date extractExpirationDate(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+}
