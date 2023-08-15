28ab9dafe5a8b536edf5ec059d7de08a3a5a49af
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-15 13:36:09 +0200

    Create custom `JwtAuthenticationFilter` before `UsernamePasswordAuthenticationFilter`.

---
+++ src/main/java/com/api/BooksApiWithMySql/config/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.api.BooksApiWithMySql.config;
+
+import io.jsonwebtoken.MalformedJwtException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException, MalformedJwtException {
+
+        final String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwtToken = authHeader.substring(7);
+
+        // TODO : extract information from token
+        String userName = jwtService.extractUserName(jwtToken);
+        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // the token is valid and not-secured yet
+            // start to extract user details
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+            if (jwtService.isValidToken(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+
+                // add more details
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                // make this user with that token SECURED
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+            filterChain.doFilter(request,response);
+        }
+
+
+    }
+}
